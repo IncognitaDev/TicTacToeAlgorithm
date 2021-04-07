0aaefcac7357e4fa5be8404b6d8f3e15ea0fb582
Luis Sousa <lv-ss@hotmail.com>
2021-04-07 09:12:25 -0300

    duplicando codigo porque não sei qual repo vai ser usado

--- index.test.js
+++ index.test.js
@@ -1,14 +1,18 @@
+const sceneryTest = ['X', 'O', '', '', 'O', 'X', '', '', '']
+
+
 const test = (script) => {
-  try{
-    const scriptReturn = script()
+  try {
+    const scriptReturn = script(sceneryTest, "X")
 
-    if (typeof scriptReturn !== 'number') return console.error('ERROR: The return is not a number')
-    if (scriptReturn > 8) return console.error('ERROR: Number is too big')
-    if (scriptReturn < 0) return console.error('ERROR: Number is too small')
-    if (scriptReturn % 1 !== 0) return console.error('ERROR: The number is not an integer')
+    if (typeof scriptReturn !== 'number') return console.error("\x1b[31m%s\x1b[0m", "\x1b[31m%s\x1b[0m", 'ERROR: The return is not a number')
+    if (scriptReturn > 8) return console.error("\x1b[31m%s\x1b[0m", 'ERROR: Number is too big')
+    if (scriptReturn < 0) return console.error("\x1b[31m%s\x1b[0m", 'ERROR: Number is too small')
+    if (scriptReturn % 1 !== 0) return console.error("\x1b[31m%s\x1b[0m", 'ERROR: The number is not an integer')
 
+    console.log("\x1b[32m%s\x1b[0m", "passed")
   } catch {
-    console.error('The script broke')
+    console.error("\x1b[31m%s\x1b[0m", 'The script broke')
   }
 }
 
